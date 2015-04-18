0a0cad1432ff4530ee408117636ec9b18b51160b
Conor Schaefer <conor.schaefer@gmail.com>
2015-04-18 13:38:20 -0700

    adds check for aptitude safe-upgrade

--- spec_tests/spec/build/packages_spec.rb
+++ spec_tests/spec/build/packages_spec.rb
@@ -1,6 +1,3 @@
-
-
-
 # declare required packages for building kernel
 required_packages = %w(
   build-essential
@@ -16,4 +13,16 @@ required_packages.each do |required_package|
   describe package(required_package) do
     it { should be_installed }
   end
+end
+
+# ensure safe-upgrade has been performed
+describe command('aptitude -s safe-upgrade') do
+  its(:exit_status) { should eq 0 }
+  expected_output = <<-eos
+No packages will be installed, upgraded, or removed.
+0 packages upgraded, 0 newly installed, 0 to remove and 0 not upgraded.
+Need to get 0 B of archives. After unpacking 0 B will be used.
+Would download/install/remove packages.
+eos
+  its(:stdout) { should eq expected_output }
 end
