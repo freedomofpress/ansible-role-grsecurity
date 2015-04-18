2c0e95695a97e203600db557d5401723c9431ea0
Conor Schaefer <conor.schaefer@gmail.com>
2015-04-18 12:52:34 -0700

    adds checks for ubuntu git dir

--- spec_tests/spec/build/build_dir_spec.rb
+++ spec_tests/spec/build/build_dir_spec.rb
@@ -1,4 +1,15 @@
 # ensure build directory is present
-describe file('/tmp/build') do
+describe file('/home/vagrant/grsec') do
   it { should be_directory }
+end
+
+# check for presence of git dir for ubuntu source
+ubuntu_git_dir = '/home/vagrant/ubuntu-trusty'
+describe file(ubuntu_git_dir) do
+  it { should be_directory }
+end
+
+# ensure git checkout completed successfully
+describe command("git fsck --git-dir #{ubuntu_git_dir}/.git --work-tree #{ubuntu_git_dir}") do
+  its(:exit_status) { should eq 0 }
 end
