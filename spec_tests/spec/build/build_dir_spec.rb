c1a7d36ccff8035cd18a1b267b75c56be9270f71
Conor Schaefer <conor.schaefer@gmail.com>
2015-04-18 12:57:03 -0700

    more git checks

--- spec_tests/spec/build/build_dir_spec.rb
+++ spec_tests/spec/build/build_dir_spec.rb
@@ -9,7 +9,15 @@ describe file(ubuntu_git_dir) do
   it { should be_directory }
 end
 
+# declare base command to re-use over several checks
+git_base_command = "git --git-dir #{ubuntu_dir_dir}/.git --work-tree #{ubuntu_git_dir}"
+
 # ensure git checkout completed successfully
-describe command("git fsck --git-dir #{ubuntu_git_dir}/.git --work-tree #{ubuntu_git_dir}") do
+describe command("#{git_base_command} fsck") do
+  its(:exit_status) { should eq 0 }
+end
+
+# ensure git tag is valid
+describe command("#{git_base_command} tag --verify $(#{git_base_command} describe)") do
   its(:exit_status) { should eq 0 }
 end
