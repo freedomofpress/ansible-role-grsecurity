10276d0e5ab4a417a0c3cd46e6404713dacd289f
Conor Schaefer <conor.schaefer@gmail.com>
2015-04-18 12:40:59 -0700

    adds sha256sums for linux kernel tarball and sig

--- spec_tests/spec/build/kernel_files_spec.rb
+++ spec_tests/spec/build/kernel_files_spec.rb
@@ -3,12 +3,12 @@ required_kernel_files = [
   {
     :filename => 'linux-3.14.21.tar.xz',
     :url => 'https://www.kernel.org/pub/linux/kernel/v3.14/linux-3.14.21.tar.xz',
-    :sha256 => '',
+    :sha256 => 'fb12f6972e115cb7ea1bfff359ca2d7cb7210a28030b8516c79ceb86009d2e26',
   },
   {
     :filename => 'linux-3.14.21.tar.sign',
     :url => 'https://www.kernel.org/pub/linux/kernel/v3.14/linux-3.14.21.tar.sign',
-    :sha256 => '',
+    :sha256 => '73e23328fac00de1fe60153e15399244b8f71996c5b9d5b087a41f01d6eb8e3d',
   },
   {
     :filename => 'grsecurity-3.0-3.14.21-201410131959.patch',
