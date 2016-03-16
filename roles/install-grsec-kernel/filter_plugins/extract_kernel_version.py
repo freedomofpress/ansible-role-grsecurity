afe63d49166b228f385abd683e81e2e5ce8b90e9
Conor Schaefer <conor.schaefer@gmail.com>
2016-03-16 11:09:27 -0700

    Creates custom filter for extracting desired kernel version
    Much more durable than using the brittle `regex_replace` filter,
    particularly because regex_replace uses different escaping formats in
    Ansible v1 and v2. Using straight Python via filters is much more
    powerful and reliable.

---
+++ roles/install-grsec-kernel/filter_plugins/extract_kernel_version.py
@@ -0,0 +1,25 @@
+import re
+
+from ansible import errors
+
+
+def extract_kernel_version(deb_package):
+    """
+    Read filename for linux-image Debian package and return only
+    the kernel version it would install, e.g. "4.4.4-grsec".
+    """
+
+    try:
+        results = re.findall(r'^linux-image-([\d.]+-grsec)', deb_package)[0]
+    except IndexError:
+        msg = ("Could not determine desired kernel version in '{}', make sure it matches "
+              "the regular expression '^linux-image-[\d.]+-grsec'").format(deb_package)
+        raise errors.AnsibleFilterError(msg)
+
+    return results
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            'extract_kernel_version': extract_kernel_version
+        }
